28737095b881a8a73bbcf1e74c90571d2dec6798
saleh-ghezelbash <80601183+saleh-ghezelbash@users.noreply.github.com>
2022-10-25 12:15:47 +0330

    Update bootcamps.js

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,16 +4,12 @@ const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
 
-// @desc      Get all bootcamps
-// @route     GET /api/v1/bootcamps
-// @access    Public
+
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   res.status(200).json(res.advancedResults);
 });
 
-// @desc      Get single bootcamp
-// @route     GET /api/v1/bootcamps/:id
-// @access    Public
+
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.id);
 
@@ -26,9 +22,7 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: bootcamp });
 });
 
-// @desc      Create new bootcamp
-// @route     POST /api/v1/bootcamps
-// @access    Private
+
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
   // Add user to req,body
   req.body.user = req.user.id;
@@ -54,9 +48,7 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc      Update bootcamp
-// @route     PUT /api/v1/bootcamps/:id
-// @access    Private
+
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
   let bootcamp = await Bootcamp.findById(req.params.id);
 
@@ -84,9 +76,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: bootcamp });
 });
 
-// @desc      Delete bootcamp
-// @route     DELETE /api/v1/bootcamps/:id
-// @access    Private
+
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.id);
 
@@ -111,20 +101,14 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   res.status(200).json({ success: true, data: {} });
 });
 
-// @desc      Get bootcamps within a radius
-// @route     GET /api/v1/bootcamps/radius/:zipcode/:distance
-// @access    Private
+
 exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
   const { zipcode, distance } = req.params;
 
-  // Get lat/lng from geocoder
   const loc = await geocoder.geocode(zipcode);
   const lat = loc[0].latitude;
   const lng = loc[0].longitude;
 
-  // Calc radius using radians
-  // Divide dist by radius of Earth
-  // Earth Radius = 3,963 mi / 6,378 km
   const radius = distance / 3963;
 
   const bootcamps = await Bootcamp.find({
@@ -138,9 +122,7 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc      Upload photo for bootcamp
-// @route     PUT /api/v1/bootcamps/:id/photo
-// @access    Private
+
 exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
   const bootcamp = await Bootcamp.findById(req.params.id);
 
