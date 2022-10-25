c4267bc587310098fe6126c5f256e4ba964f562b
saleh-ghezelbash <80601183+saleh-ghezelbash@users.noreply.github.com>
2022-10-25 12:13:33 +0330

    Update courses.js

--- controllers/courses.js
+++ controllers/courses.js
@@ -3,10 +3,6 @@ const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
 const Bootcamp = require('../models/Bootcamp');
 
-// @desc      Get courses
-// @route     GET /api/v1/courses
-// @route     GET /api/v1/bootcamps/:bootcampId/courses
-// @access    Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
   if (req.params.bootcampId) {
     const courses = await Course.find({ bootcamp: req.params.bootcampId });
@@ -21,9 +17,7 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   }
 });
 
-// @desc      Get single course
-// @route     GET /api/v1/courses/:id
-// @access    Public
+
 exports.getCourse = asyncHandler(async (req, res, next) => {
   const course = await Course.findById(req.params.id).populate({
     path: 'bootcamp',
@@ -42,9 +36,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc      Add course
-// @route     POST /api/v1/bootcamps/:bootcampId/courses
-// @access    Private
+
 exports.addCourse = asyncHandler(async (req, res, next) => {
   req.body.bootcamp = req.params.bootcampId;
   req.body.user = req.user.id;
@@ -78,9 +70,7 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc      Update course
-// @route     PUT /api/v1/courses/:id
-// @access    Private
+
 exports.updateCourse = asyncHandler(async (req, res, next) => {
   let course = await Course.findById(req.params.id);
 
@@ -113,9 +103,7 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
   });
 });
 
-// @desc      Delete course
-// @route     DELETE /api/v1/courses/:id
-// @access    Private
+
 exports.deleteCourse = asyncHandler(async (req, res, next) => {
   const course = await Course.findById(req.params.id);
 
